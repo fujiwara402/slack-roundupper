b338d2bb4b9695153035f1dd29f0f6db2bb89351
fujiwara402 <fuj.t601216@gmail.com>
2016-06-10 16:32:00 +0900

    .gitignore is not working

--- chatmessage2mdbot.go
+++ chatmessage2mdbot.go
@@ -10,27 +10,33 @@ import (
 )
 
 func unescapeCharacters(escaped_message string) string {
+
 	r := strings.NewReplacer("&amp;", "&",
 		"&lt;", "<",
-		"&gt;", ">")
+		"&gt;", ">",
+		"<", "",
+		">", "")
 	message := r.Replace(escaped_message)
 	return message
 }
 
-func convertIdToName(include_id_message string) string {
+func id2Name(include_id_message string) string {
 	r := strings.NewReplacer("U0BRJDKKN", "t_fujiwara")
 	message := r.Replace(include_id_message)
 	return message
 }
 
-func main() {
+func init() {
 	api := slack.New(os.Getenv("SLACK_API_TOKEN"))
+}
+
+func main() {
 	params := slack.HistoryParameters{Count: 100}
 	history, _ := api.GetChannelHistory("C066ZLMJS", params)
 	for _, msg := range history.Messages {
 		text := unescapeCharacters(msg.Text)
-		text = convertIdToName(text)
-		id := convertIdToName(msg.User)
+		text = id2Name(text)
+		id := id2Name(msg.User)
 		time_stamp := strings.Split(msg.Timestamp, ".")
 		utime, _ := strconv.ParseInt(time_stamp[0], 10, 64)
 		fmt.Printf("ID:%v, Message:%v, Timestamp:%v\n", id, text, time.Unix(utime, 0))
